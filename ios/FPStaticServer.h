0bc647abca568194065d1ac81a3bae00dae89cbd
radwanaltaf <radwanaltaf2000@gmail.com>
2021-03-16 05:16:09 +0800

    Realm Conflict Fix

---
+++ ios/FPStaticServer.h
@@ -0,0 +1,21 @@
+#import <React/RCTBridgeModule.h>
+
+// GCDWebServer: https://github.com/swisspol/GCDWebServer
+//#import <RealmReact/Libraries/RealmJS/Vendor/GCDWebServer.h>
+#import <../../realm/vendor/GCDWebServer/GCDWebServer/Core/GCDWebServer.h>
+
+
+@interface FPStaticServer : NSObject <RCTBridgeModule> {
+    GCDWebServer* _webServer;
+}
+
+    @property(nonatomic, retain) NSString *localPath;
+    @property(nonatomic, retain) NSString *url;
+
+    @property (nonatomic, retain) NSString* www_root;
+    @property (nonatomic, retain) NSNumber* port;
+    @property (assign) BOOL localhost_only;
+    @property (assign) BOOL keep_alive;
+
+@end
+  
